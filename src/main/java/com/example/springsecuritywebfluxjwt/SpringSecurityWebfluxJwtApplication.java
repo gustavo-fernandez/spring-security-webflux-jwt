4080418e17f904dc205283bb741466bfab748ab0
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2022-01-11 19:22:24 -0500

    Project setup

---
+++ src/main/java/com/example/springsecuritywebfluxjwt/SpringSecurityWebfluxJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritywebfluxjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWebfluxJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWebfluxJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecuritywebfluxjwt/SpringSecurityWebfluxJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritywebfluxjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWebfluxJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
